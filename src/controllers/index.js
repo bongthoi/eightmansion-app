8ac4592f4d917df30f8dbbb0b8fa10b8002bbebd
bongthoi <bongthoi>
2019-01-02 10:13:26 +0700

    1.3- update vietnamese language

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -5,15 +5,15 @@ let router = express.Router();
 
 /**public */
 router.get("/", (req, res) => {
-	res.render("index", { title: "Home" });
+	res.render("index", { title: "Trang Chủ" });
 });
 
 router.get("/explanation", (req, res) => {
-	res.render("explanation", { title: "Explanation" });
+	res.render("explanation", { title: "Giải Trình" });
 });
 
 router.get("/solution", (req, res) => {
-	res.render("solution", { title: "Solution" });
+	res.render("solution", { title: "Phương Pháp Hóa Giải" });
 });
 
 /**export */
